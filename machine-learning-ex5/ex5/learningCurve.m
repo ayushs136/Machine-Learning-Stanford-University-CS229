c542affbc287d2c5bcdae3a4ce81a85f6c07ef28
AYUSH SHARMA <ayushs136@gmail.com>
2019-02-04 01:49:08 +0530

    added week 6

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -42,13 +42,21 @@ error_val   = zeros(m, 1);
 %
 % Hint: You can loop over the examples with the following:
 %
-%       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
-%       end
+
+      for i = 1:m
+      	  % theta(i)  = trainLinearReg(X(i), y(i), lambda);
+          % Compute train/cross validation errors using training examples 
+          % X(1:i, :) and y(1:i), storing the result in 
+          % error_train(i) and error_val(i)
+          X_train = X(1:i, :);
+          y_train = y(1:i);
+
+          theta = trainLinearReg(X_train, y_train, lambda);
+
+          error_train(i) = linearRegCostFunction(X_train, y_train, theta, 0)
+          error_val(i) =linearRegCostFunction(Xval, yval,theta, 0); 
+          
+      end
 %
 
 % ---------------------- Sample Solution ----------------------
--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -13,12 +13,13 @@ X_poly = zeros(numel(X), p);
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
-% 
 
 
 
-
-
+for i = 1:p,
+	X_poly(:, i) = X(:,1).^i;
+end
+% X_poly(:, 1:p) = X(:, 1).&(1:p);
 
 % =========================================================================
 
